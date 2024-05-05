c24c7a2a45477ebbc21f663215ee566b4597915a
dumisa <dumisa@crocs.co.za>
2024-05-05 17:44:03 +0200

    new file:   ns-allinone-3.41/ns-3.41/sim_scripts/sim_basic_stoch_bw.sh

---
+++ ns-allinone-3.41/ns-3.41/sim_scripts/sim_basic_stoch_bw.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ..
+
+./ns3 run "examples/tcp/tcp_qt_optimal_sims --transport_prot="TcpBbr" --sim_name="../../results/stochastic_bw" --num_flows=1 --stochBW=true --btlBW="10Mbps" --btlDelay="100ms" --sim_duration=200"
+./ns3 run "examples/tcp/tcp_qt_optimal_sims --transport_prot="TcpCubic" --sim_name="../../results/stochastic_bw" --num_flows=1 --stochBW=true --btlBW="10Mbps" --btlDelay="100ms" --sim_duration=200"
+./ns3 run "examples/tcp/tcp_qt_optimal_sims --transport_prot="TcpQtOptimal" --Rt_mult=1.1 --sim_name="../../results/stochastic_bw" --num_flows=1 --stochBW=true --btlBW="10Mbps" --btlDelay="100ms" --sim_duration=200"
+./ns3 run "examples/tcp/tcp_qt_optimal_sims --transport_prot="TcpQtOptimal" --Rt_mult=1.2 --sim_name="../../results/stochastic_bw" --num_flows=1 --stochBW=true --btlBW="10Mbps" --btlDelay="100ms" --sim_duration=200"
+./ns3 run "examples/tcp/tcp_qt_optimal_sims --transport_prot="TcpQtOptimal" --Rt_mult=1.3 --sim_name="../../results/stochastic_bw" --num_flows=1 --stochBW=true --btlBW="10Mbps" --btlDelay="100ms" --sim_duration=200"
+./ns3 run "examples/tcp/tcp_qt_optimal_sims --transport_prot="TcpQtOptimal" --Rt_mult=1.4 --sim_name="../../results/stochastic_bw" --num_flows=1 --stochBW=true --btlBW="10Mbps" --btlDelay="100ms" --sim_duration=200"
+./ns3 run "examples/tcp/tcp_qt_optimal_sims --transport_prot="TcpQtOptimal" --Rt_mult=1.5 --sim_name="../../results/stochastic_bw" --num_flows=1 --stochBW=true --btlBW="10Mbps" --btlDelay="100ms" --sim_duration=200"
